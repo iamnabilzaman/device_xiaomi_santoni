8b9e3672948352d4b721f0c3aad9dfd3223b6649
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-21 21:54:52 +0600

    santoni: Updated for SparkOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,35 +1,8 @@
-# ArrowOS
+# SparkOS
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_santoni.mk
+    $(LOCAL_DIR)/spark_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_santoni-userdebug \
-    arrow_santoni-user \
-    arrow_santoni-eng
-
-# AEX
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_santoni-userdebug \
-    aosp_santoni-user \
-    aosp_santoni-eng
-
-# Octavi-OS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/octavi_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    octavi_santoni-userdebug \
-    octavi_santoni-user \
-    octavi_santoni-eng
-
-# Syberia-Project
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/syberia_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    syberia_santoni-userdebug \
-    syberia_santoni-user \
-    syberia_santoni-eng
+    spark_santoni-userdebug \
+    spark_santoni-user \
+    spark_santoni-eng
