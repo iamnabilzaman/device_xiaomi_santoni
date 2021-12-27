8694e8d4c414151e9440ba6b857080fd3864e472
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-28 01:01:33 +0600

    santoni: Added support for POSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# ArrowOS
+# POSP
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_santoni.mk
+    $(LOCAL_DIR)/potato_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    komodo_santoni-userdebug \
-    komodo_santoni-user \
-    komodo_santoni-eng
+    potato_santoni-userdebug \
+    potato_santoni-user \
+    potato_santoni-eng
